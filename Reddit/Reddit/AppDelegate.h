8837bdce664f2a7076e371a1e7f00d8479365538
William Moody <williammoody@Williams-MacBook-Air.local>
2019-05-22 10:32:53 -0600

    first commit

---
+++ Reddit/Reddit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reddit
+//
+//  Created by William Moody on 5/22/19.
+//  Copyright © 2019 William Moody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Reddit/RedditTests/RedditTests.m
@@ -0,0 +1,37 @@
+//
+//  RedditTests.m
+//  RedditTests
+//
+//  Created by William Moody on 5/22/19.
+//  Copyright © 2019 William Moody. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RedditTests : XCTestCase
+
+@end
+
+@implementation RedditTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
